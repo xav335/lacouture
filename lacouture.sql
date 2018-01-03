86b99a758b1fbbdd4c30537be83ed1d5269900cd
J. Gonzalez <fjavi.gonzalez@gmail.com>
2018-01-03 13:13:48 +0100

    avant MEP + DB

--- lacouture.sql
+++ lacouture.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.21, for osx10.6 (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.58, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: lacouture
 -- ------------------------------------------------------
--- Server version	5.6.21
+-- Server version	5.5.58-0+deb8u1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -240,4 +240,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-02  0:55:41
+-- Dump completed on 2018-01-03 13:13:11
